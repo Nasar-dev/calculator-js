b05e9f23386c4e9372fdae43d9d52633a0b2c009
Nasar Shahzad <nasarshahzad45@gmail.com>
2026-06-08 19:52:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        let value = button.innerText;
+
+        if(value === "C"){
+            display.value = "";
+        }
+
+        else if(value === "="){
+            display.value = eval(display.value);
+        }
+
+        else{
+            display.value += value;
+        }
+
+    });
+
+});
